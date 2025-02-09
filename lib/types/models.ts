dceb96a56a4765fc0021b47cf59061ca1e82ac97
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-08 19:02:31 -0800

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,32 +8,26 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-2.0-pro-exp-02-05',
-    name: 'ELITE FLOW (G 2.0 Pro (Exp)',
-    provider: '',
+    name: 'Elite flow 2.0 Pro (Exp)',
+    provider: 'Google Gemini API-powered',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash-thinking-exp-01-21',
-    name: 'PATOOPACE RBP (G 2.0 Flash Thinking (Exp)',
-    provider: '',
+    name: 'Patoopace 2.0 Flash Thinking (Exp)',
+    provider: 'Google Gemini API-powered',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash',
-    name: 'Elevation Pace (G 2.0-flash)',
-    provider: '',
+    name: 'ElevationPace  2.0-flash)',
+    provider: 'Google Gemini API-powered',
     providerId: 'google'
   },
   {
     id: 'gemini-1.5-pro-002',
-    name: 'SOLIDSPACE (G 1.5 pro)',
-    provider: '',
+    name: 'SolidSpace 1.5 pro)',
+    provider: 'Google Gemini API-powered',
     providerId: 'google'
-  },
-  {
-    id: 'deepseek-r1-distill-llama-70b',
-    name: '99PACE (Groq-llama-70b)',
-    provider: 'GOOGLE GEMINI Family of Models',
-    providerId: 'groq'
   }
 ]
