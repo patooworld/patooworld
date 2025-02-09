08339304aa3ae8edca9f47f6b90592e72154db88
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-08 19:45:30 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiLinkedin, SiGithub, SiX } from 'react-icons/si'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
@@ -12,8 +12,8 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
+          <Link href="https://linkedin.com/in/patooworld" target="_blank">
+            <SiLinkedin size={18} />
           </Link>
         </Button>
         <Button
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://x.com/morphic_ai" target="_blank">
+          <Link href="https://x.com/patooworld" target="_blank">
             <SiX size={18} />
           </Link>
         </Button>
@@ -30,7 +30,7 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://git.new/morphic" target="_blank">
+          <Link href="https://git.new/patooworld" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
