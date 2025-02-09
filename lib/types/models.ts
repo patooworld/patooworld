b0bd55b40e5205b8b3e05bb2b099c7da523f7f9a
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-08 19:38:53 -0800

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,26 +8,32 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-2.0-pro-exp-02-05',
-    name: 'Elite flow 2.0 Pro (Exp)',
+    name: 'Cloudinpace 2.0 Pro (PW)',
     provider: 'Google Gemini API-powered',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash-thinking-exp-01-21',
-    name: 'Patoopace 2.0 Flash Thinking (Exp)',
+    name: 'PatooPace 2.0 Flash Thinking (PW)',
     provider: 'Google Gemini API-powered',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash',
     name: 'ElevationPace  2.0-flash)',
-    provider: 'Google Gemini API-powered',
+    provider: 'Google Gemini API-powered (PW)',
     providerId: 'google'
   },
   {
     id: 'gemini-1.5-pro-002',
     name: 'SolidSpace 1.5 pro)',
-    provider: 'Google Gemini API-powered',
+    provider: 'Google Gemini API-powered (PW)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-flash-exp',
+    name: '99PACE',
+    provider: 'Google Gemini API-powered (PW)',
     providerId: 'google'
   }
 ]
