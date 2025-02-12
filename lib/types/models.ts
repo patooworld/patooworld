f93366bbc28db5d95f7c5783818af72f21f7bc31
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-12 10:23:11 -0800

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -7,27 +7,9 @@ export interface Model {
 
 export const models: Model[] = [
   {
-    id: 'gemini-2.0-pro-exp-02-05',
-    name: 'Gemini 2.0 Pro (Exp)',
-    provider: 'Google Generative AI',
-    providerId: 'google'
-  },
-  {
-    id: 'gemini-2.0-flash-thinking-exp-01-21',
-    name: 'Gemini 2.0 Flash Thinking (Exp)',
-    provider: 'Google Generative AI',
-    providerId: 'google'
-  },
-  {
     id: 'gemini-2.0-flash',
-    name: 'Gemini 2.0-flash',
-    provider: 'Google Generative AI',
+    name: 'PATOOWORLD',
+    provider: 'PATOOWORLD',
     providerId: 'google'
   },
-  {
-    id: 'gemini-1.5-pro-002',
-    name: 'Gemini 1.5 pro',
-    provider: 'Google Generative AI',
-    providerId: 'google'
-  }
 ]
