dabc78c8541207128e2c630afa8623df67199394
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-12 10:32:40 -0800

    Update footer.tsx
    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,7 +30,7 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://git.new/patooworld" target="_blank">
+          <Link href="https://github.com/patooworld" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
