f1a2740f3635b0ce3976fa87c0d7b207ef722752
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-13 05:04:02 -0800

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
+import { Spinner } from './spinner'
+import { cn } from '@/lib/utils'
 import { cva, type VariantProps } from 'class-variance-authority'
 
-import { cn } from '@/lib/utils'
-
+export const buttonBase = 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
@@ -37,17 +38,26 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, isLoading, disabled, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
+    const isDisabled = disabled || isLoading
+    
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonBase, buttonVariants({ variant, size, className }), {
+          'pointer-events-none opacity-50': isDisabled,
+        })}
+        disabled={isDisabled}
         ref={ref}
         {...props}
-      />
+      >
+      <img className='https://avatars.githubusercontent.com/u/114192133?s=400&u=bc3be2911373de03dd38cd3ad23819a4879af825&v=4' />
+        {props.children}
+      </Comp>
     )
   }
 )
