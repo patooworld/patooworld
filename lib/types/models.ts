e27b70ef6a8fc9ad701ab12e7e09d5761a82dbc2
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-13 07:49:26 -0800

    Update models.ts
    google models listing. disregard renames

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -9,7 +9,31 @@ export const models: Model[] = [
   {
     id: 'gemini-2.0-flash',
     name: 'PATOOWORLD',
-    provider: 'PATOOWORLD',
+    provider: 'Gemini AI-powered world',
+    providerId: 'google'
+  },
+ {
+    id: 'gemini-2.0-pro-exp-02-05',
+    name: 'PATOOWORLD CV pro',
+    provider: 'Gemini 2.0 Pro (Exp)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-pro-exp-01-21',
+    name: 'Elite Cyb',
+    provider: 'Gemini 2.0 Pro (Exp)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-flash-thinking-exp-01-21',
+    name: 'PATOOWORLD C Vision',
+    provider: 'Gemini 2.0 Flash Thinking (Exp)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-1.5-pro-002',
+    name: 'PATOOWORLD LLM Pro',
+    provider: 'Gemini 1.5 pro',
     providerId: 'google'
   },
 ]
