7db537acb904ee3d4dc2a4297adbbf0db8d9d172
SAMUEL CHIBUNNA <info@patooworld.com>
2025-02-26 16:33:28 -0800

    Update models.ts
    models extended..

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,8 +8,26 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-2.0-flash',
-    name: 'PATOOWORLD',
-    provider: 'PATOOWORLD AI (TaskMaster Assistant)',
+    name: 'PATOOWORLD CA',
+    provider: 'Conversational AI (Speed and Efficiency)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-1.5-pro-002',
+    name: 'PATOOWORLD DL',
+    provider: 'Deep Learning (PW AI based on Choice)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-pro-exp-02-05',
+    name: 'PATOOWORLD ML',
+    provider: 'Machine Learning (Exp)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-flash-thinking-exp-01-21',
+    name: 'PATOOWORLD CV',
+    provider: 'Computer Vision (Exp)',
     providerId: 'google'
   },
 ]
