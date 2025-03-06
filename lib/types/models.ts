8e2977e36063b124552ffc503422b974311f236b
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-06 10:12:56 -0800

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -7,9 +7,27 @@ export interface Model {
 
 export const models: Model[] = [
   {
+    id: 'gemini-2.0-flash',
+    name: 'PATOOWORLD UBA',
+    provider: 'Unified Business AI (Speed and Efficiency)',
+    providerId: 'google'
+  },
+  {
     id: 'gemini-1.5-pro-002',
-    name: 'PATOOWORLD',
-    provider: 'PATOOWORLD (Search, Research & Generate. Your Choice, We Adore.)',
+    name: 'PATOOWORLD DL',
+    provider: 'Deep Learning (based on Choice)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-pro-exp-02-05',
+    name: 'PATOOWORLD ML',
+    provider: 'Machine Learning (Exp)',
+    providerId: 'google'
+  },
+  {
+    id: 'gemini-2.0-flash-thinking-exp-01-21',
+    name: 'PATOOWORLD CV',
+    provider: 'Computer Vision (Exp)',
     providerId: 'google'
   },
 ]
