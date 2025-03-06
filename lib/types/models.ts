7c2977665e09d752a50374551b4e64c89833dc36
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-06 02:03:49 -0800

    Update models.ts
    Renamed model

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,14 +8,8 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-1.5-pro-002',
-    name: 'ATOLOG-PA',
-    provider: 'PATOOWORLD AI',
-    providerId: 'google'
-  },
-  {
-    id: 'gemini-2.0-flash',
-    name: 'ATOLOG-AG',
-    provider: 'PATOOWORLD AI',
+    name: 'ATOLOG',
+    provider: 'PATOOWORLD',
     providerId: 'google'
   },
 ]
