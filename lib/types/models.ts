d78f5c78f111785d5bee449a5555def3a6a1eb5e
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-08 13:01:54 -0800

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -6,31 +6,6 @@ export interface Model {
 }
 
 export const models: Model[] = [
-  {
-    id: 'gemini-2.0-flash',
-    name: 'PATOOWORLD AI2',
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:1622166260.
-    provider: 'Deep Learning (Speed and Efficiency in generating text, images, audio, and video)',
-    providerId: 'google'
-  },
-  {
-    id: 'gemini-1.5-pro-002',
-    name: 'PATOOWORLD SR',
-    provider: 'Deep Dive AI-driven Tools(Search, poems, code, scripts, musical pieces, email, Study and Research In-depth understanding of any subject based on Choice etc.)',
-    providerId: 'google'
-  },
-  {
-    id: 'gemini-2.0-pro-exp-02-05',
-    name: 'ATOLOG Pcxrm',
-    provider: 'Deep Dive (Research, and Code Investigator with Detailed analysis in Application development, Cloud base historical events.etc(Exp)',
-    providerId: 'google'
-  },
-  {
-    id: 'gemini-2.0-flash-thinking-exp-01-21',
-    name: 'PATOOWORLD CV',
-    provider: 'Deep Learning (Algorithmic trained on large datasets for critical thinking in Computer Vision(Exp)',
-    providerId: 'google'
-  },
  {
   id: 'deepseek-r1-distill-llama-70b',
   name: '99PACE',
