918e3e899c381f0fab62040d7e6f5e87ef5a8b6d
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-07 18:13:52 -0800

    Update models.ts
    enhanced descriptions to understand functionalities based perspectives

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,26 +8,26 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-2.0-flash',
-    name: 'PATOOWORLD CA',
-    provider: 'Conversational AI (Speed and Efficiency)',
+    name: 'PATOOWORLD Bot',
+    provider: 'Deep Learning (Data analysis, Marketing Vision, pattern recognition Speed and Efficiency)',
     providerId: 'google'
   },
   {
     id: 'gemini-1.5-pro-002',
-    name: 'PATOOWORLD DL',
-    provider: 'Deep Learning (PW AI based on Choice)',
+    name: 'PATOOWORLD SR',
+    provider: 'Deep Dive (Study and Research In-depth understanding of any subject based on Choice)',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-pro-exp-02-05',
-    name: 'PATOOWORLD ML',
-    provider: 'Machine Learning (Exp)',
+    name: 'PATOOWORLD RS',
+    provider: 'Deep Dive (Research, and Code Investigator with Detailed analysis in Application development, Cloud base historical events.etc(Exp)',
     providerId: 'google'
   },
   {
     id: 'gemini-2.0-flash-thinking-exp-01-21',
     name: 'PATOOWORLD CV',
-    provider: 'Computer Vision (Exp)',
+    provider: 'Deep Learning (Algorithmic trained on large datasets for critical thinking in Computer Vision(Exp)',
     providerId: 'google'
   },
 ]
