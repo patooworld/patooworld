a8240dccc0ffd603a200dad9b67bb54c9f5e3e07
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-07 17:49:30 -0800

    Update models.ts
    complete fix

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -8,7 +8,7 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-2.0-flash',
-    name: 'PATOOWORLD ',
+    name: 'PATOOWORLD Bot',
     provider: 'Deep Learning (Data analysis, Marketing Vision, pattern recognition Speed and Efficiency)',
     providerId: 'google'
   },
