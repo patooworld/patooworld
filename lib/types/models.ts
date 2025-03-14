552cbf920bd2657123e869bd01714c33a70338ea
SAMUEL CHIBUNNA <info@patooworld.com>
2025-03-13 17:22:12 -0700

    Update models.ts
    extended clarity

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -9,19 +9,19 @@ export const models: Model[] = [
     {
     id: 'gemini-1.5-pro-002',
     name: 'PATOOWORLD',
-    provider: 'PATOOWORLD AI {Deep Learning developers tool. Search, Research and write code like a pro based on choice}',
+    provider: 'Deep Learning {Study Research and write code, poem and complete tasks like a pro based on choice}',
     providerId: 'google'
    },
    {
     id: 'gemini-2.0-flash',
-    name: '99PACE',
-    provider: 'PATOOWORLD AI2 {Generate, Reform and Transfrom any Video, Audio and Image with speed of Light}',
+    name: 'ELEVATION',
+    provider: 'Deep Learning {Generate, Reform and Transfrom any Video, Audio and Image with speed of Light}',
     providerId: 'google'
    },
    {
     id: 'gemini-2.0-flash-thinking-exp-01-21',
-    name: 'ELEVATION', 
-    provider: 'PATOOWORLD CV {Deep Learning In-depth understanding of any subject from Computer Vision.}',
+    name: '99PACE', 
+    provider: 'DeepMind {Deep Dive In-depth understanding of any subject from Computer Vision to Cybersecurity etc..}',
     providerId: 'google'
    },
 ]
